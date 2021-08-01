11597c6ddfbc78a610e84118645a81b72328bcdd
JJAHOO <wogns0197@naver.com>
2021-08-01 21:55:16 +0900

    Publish

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import { ClientData } from '../type';
 import axios from "axios";
 
 export const registerClientData = async (inData: ClientData) => {
-  await axios.post('http://localhost:5000/api/registerData', { ...inData })
+  await axios.post('http://34.64.229.15:5000/api/registerData', { ...inData })
     .then(res => {
       console.log(res);
   })
@@ -13,7 +13,7 @@ export const registerClientData = async (inData: ClientData) => {
 
 export const updateClientData = async (inData: ClientData) => {
   // inData = { phonenumber, point }
-  await axios.post('http://localhost:5000/api/updateData', { ...inData })
+  await axios.post('http://34.64.229.15:5000/api/updateData', { ...inData })
   .then(res => {
     console.log(res);
   })
@@ -23,6 +23,6 @@ export const updateClientData = async (inData: ClientData) => {
 }
 
 export const getClientData = async (phonenumber: string) => {
-  const res = await axios.post('http://localhost:5000/api/getData', { phonenumber });
+  const res = await axios.post('http://34.64.229.15:5000/api/getData', { phonenumber });
   return res.data;  
 }
