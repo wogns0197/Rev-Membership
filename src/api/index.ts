f642fd7e7fd2c35d8ee8d464f47404a7471b0987
JJAHOO <wogns0197@naver.com>
2021-08-04 22:58:24 +0900

    아이코 실수

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,6 @@
 import { ClientData } from '../type';
 import axios from "axios";
 
-export const registerClientData = async (inData: ClientData) => {
-  await axios.post('http://localhost:5000/api/registerData', { ...inData })
-  // await axios.post('http://34.64.229.15:5000/api/registerData', { ...inData })
-    .then(res => {
-      console.log(res);
-  })
-    .catch(err => {
-      console.log(err);
-  })
-}
-
 interface UpdateDataType{
   phonenumber: string,
   point: number,
@@ -22,6 +11,16 @@ interface UpdateDataType{
   buycount?: number,
 }
 
+export const registerClientData = async (inData: ClientData) => {
+  // await axios.post('http://localhost:5000/api/registerData', { ...inData })
+  await axios.post('http://34.64.229.15:5000/api/registerData', { ...inData })
+    .then(res => {
+      console.log(res);
+  })
+    .catch(err => {
+      console.log(err);
+  })
+}
 
 export const updateClientData = async (inData: UpdateDataType) => {
   // inData = { phonenumber, point }
